6cccaa98227ad2070b7986d1848f40709b8ef7d4
Jason Gao <jasongao678@gmail.com>
2023-10-12 23:53:30 -0400

    Fix info page for mobile scrolling

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -20,13 +20,14 @@ const StyledHelp = styled.div`
     display: flex;
     /* align-items: center; */
     /* justify-content: center; */
+    overflow: auto;
 
     .container {
         /* background: slategray; */
         color: lightgrey;
         
         width: ${window.innerWidth < window.innerHeight ? "100vw" : "calc(75vw - 50px)"};
-        height: 100%;
+        height: calc(100% - 60px);
         z-index: 3000;
 
         display: flex;
@@ -34,7 +35,12 @@ const StyledHelp = styled.div`
         justify-content: space-between;
 
         margin: auto;
+
         padding: 30px 30px 30px 30px;
+
+        .text {
+            
+        }
 
         button {
             color: lightgrey;
@@ -59,19 +65,21 @@ const Help = ({hideHelp}: {hideHelp: () => void}) => {
                     This app was built with React.js.
                     <br/>
                     <br/>
-                    The backend uses the WhisperX speech-to-text transcription model to align the times of every word in the lyrics.
+                    The backend uses the WhisperX speech-to-text transcription model to align the times of the lyrics.
                     Deezer's Spleeter source separation model was used to remove the vocals from the music.
                     <br />
                     <br />
-                    The app isn't perfect. Sometimes WhisperX will drop some audio, which will desync the lyrics. 
-                    Also, right now the app only supports English songs. Hopefully you can still get some fun out of it.
+                    The app isn't perfect. Sometimes WhisperX will drop some audio and desync the lyrics, 
+                    and right now the app only supports English songs. Hopefully you can still get some fun out of it.
                     <br />
                     <br />
-                    If you like the app, you can check out my site <a href="https://myriadrivers.github.io/">here</a>, 
-                    and if you find any bugs, let me know at jasongao678@gmail.com.
+                    If you find any bugs, let me know at jasongao678+dev (at) gmail.com, 
+                    and if you like the app, you can check out my site <a href="https://myriadrivers.github.io/">here</a>, 
                     <br />
                     <br />
                     —Jason
+                    <br />
+                    <br />
                 </div>
                 <button onClick={hideHelp}><FontAwesomeIcon icon={faXmark} size={"2x"}/></button>
             </div>
