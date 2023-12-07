3c48c2ae35f86ba4385bc39e2e325d5e68810993
Jason Gao <jasongao678@gmail.com>
2023-12-06 20:37:42 -0500

    Configure auth with awsexports

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,14 @@ import SongSearch from './components/SongSearch/SongSearch';
 import LyricsDisplay from './components/LyricsDisplay';
 import { Word } from './types';
 import TitleBar from './components/TitleBar';
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import awsExports from "./aws-exports"
 
 const client_id = '269d6b0c0ffe4e32b0d055155b0f8e82';
 const redirect_uri = "https://main.d1tjf0r777xvgj.amplifyapp.com/"
 
 Amplify.configure(awsExports);
+Auth.configure(awsExports);
 
 const api = SpotifyApi.withUserAuthorization(
   client_id, redirect_uri
